ee6a8fe4ab1e140ea5512eb3383e5e0089027c96
Joris Oosterhuis (DESKTOP) <joris.oosterhuis007@gmail.com>
2017-10-12 17:20:35 +0200

    Implemented Fabric Crashlytics

--- app/src/main/java/nl/easthome/ebikereader/LoginActivity.java
+++ app/src/main/java/nl/easthome/ebikereader/LoginActivity.java
@@ -8,11 +8,13 @@ import android.widget.RelativeLayout;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import com.crashlytics.android.Crashlytics;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
+import io.fabric.sdk.android.Fabric;
 
 import java.util.Arrays;
 
@@ -37,6 +39,7 @@ public class LoginActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Fabric.with(this, new Crashlytics());
 		setContentView(R.layout.activity_login);
 		FirebaseApp.initializeApp(this);
 		ButterKnife.bind(this);
